6d30926f15b0eade70548a71d626109d277ec180
Maria Salama <m.salama@cs.bham.ac.uk>
2023-06-07 17:14:45 +0100

    Add files via upload

---
+++ src/org/cloudbus/iotnetsim/iot/nodes/MessagingProtocol.java
@@ -0,0 +1,18 @@
+package org.cloudbus.iotnetsim.iot.nodes;
+
+/**
+ * @author m.salama
+ *
+ */
+public enum MessagingProtocol {
+	TCP,
+	FTP,
+	HTTP,
+	HTTPS,
+	TLS,
+	PPP, 
+	SOAP,
+	MQTT, 
+	V2X //Vehicle-to-Everything
+
+}
